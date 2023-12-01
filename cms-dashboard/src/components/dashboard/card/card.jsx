5e658c25570f9691673f93781fd34cf2115c132c
Andry Ariadi <andryariadi23@gmail.com>
2023-12-01 23:26:30 +0700

    styling card

--- cms-dashboard/src/components/dashboard/card/card.jsx
+++ cms-dashboard/src/components/dashboard/card/card.jsx
@@ -1,7 +1,19 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
 export default function Card() {
   return (
     <>
-      <h1>Card Components</h1>
+      <div className={styles.container}>
+        <MdSupervisedUserCircle />
+        <div className={styles.texts}>
+          <span className={styles.titleUser}>Total Users</span>
+          <span className={styles.value}>10.273</span>
+          <span className={styles.detail}>
+            <span className={styles.positive}>12%</span>
+            more than previous week
+          </span>
+        </div>
+      </div>
     </>
   );
 }
