c8271a79bfe155e82a3bb63029c6840e44fabbca
Andry Ariadi <andryariadi23@gmail.com>
2023-12-01 21:34:20 +0700

    styling navbar

--- cms-dashboard/src/components/dashboard/navbar/navbar.jsx
+++ cms-dashboard/src/components/dashboard/navbar/navbar.jsx
@@ -1,7 +1,27 @@
+"use client";
+import { usePathname } from "next/navigation";
+import styles from "./navbar.module.css";
+import { MdNotifications, MdOutlineChat, MdPublic, MdSearch } from "react-icons/md";
+
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <>
-      <h1>Navbar Component</h1>
+      <div className={styles.container}>
+        <div className={styles.title}>{pathname.split("/").pop()}</div>
+        <div className={styles.menuList}>
+          <div className={styles.search}>
+            <MdSearch />
+            <input type="text" placeholder="Search..." className={styles.input} />
+          </div>
+          <div className={styles.icon}>
+            <MdOutlineChat size={20} />
+            <MdNotifications size={20} />
+            <MdPublic size={20} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
