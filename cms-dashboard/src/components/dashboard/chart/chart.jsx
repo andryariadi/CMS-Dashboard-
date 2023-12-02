4bec881effedc89ef2bbf9223210349edaa2e92a
Andry Ariadi <andryariadi23@gmail.com>
2023-12-02 22:28:22 +0700

    styling rightbar

--- cms-dashboard/src/components/dashboard/chart/chart.jsx
+++ cms-dashboard/src/components/dashboard/chart/chart.jsx
@@ -1,6 +1,6 @@
 "use client";
 import styles from "./chart.module.css";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 const data = [
   {
@@ -45,7 +45,7 @@ export default function Chart() {
     <>
       <div className={styles.mainContainer}>
         <div className={styles.container}>
-          <h2>Weekly Recap</h2>
+          <h2 className={styles.title}>Weekly Recap</h2>
           <ResponsiveContainer width="100%" height="100%">
             <LineChart
               width={500}
@@ -58,10 +58,9 @@ export default function Chart() {
                 bottom: 5,
               }}
             >
-              <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="name" />
               <YAxis />
-              <Tooltip />
+              <Tooltip contentStyle={{ backgroundColor: "#151c2c", border: "none" }} />
               <Legend />
               <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
               <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
--- cms-dashboard/src/components/dashboard/rightbar/rightbar.jsx
+++ cms-dashboard/src/components/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,40 @@
+import styles from "./rightbar.module.css";
+import Image from "next/image";
+import astronaut from "@/assets/astronaut.png";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
+
 export default function Rightbar() {
   return (
     <>
-      <h1>Rightbar Components</h1>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.bgImage}>
+            <Image src={astronaut} alt="astro" fill className={styles.image} />
+          </div>
+          <div className={styles.texts}>
+            <span className={styles.notification}>🔥 Available Now</span>
+            <h3 className={styles.title}>How to use the new version of the admin dashboard?</h3>
+            <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+            <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius libero perspiciatis recusandae possimus.</p>
+            <button className={styles.button}>
+              <MdPlayCircleFilled />
+              Watch
+            </button>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.text}>
+            <span className={styles.notification}>🚀 Coming Soon</span>
+            <h3 className={styles.title}>New server actions are available, partial pre-rendering is coming up!</h3>
+            <span className={styles.subtitle}>Boost your productivity</span>
+            <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius libero perspiciatis recusandae possimus.</p>
+            <button className={styles.button}>
+              <MdReadMore />
+              Learn
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
