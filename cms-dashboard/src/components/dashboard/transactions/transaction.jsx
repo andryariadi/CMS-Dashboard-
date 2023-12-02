ac56b20623ce2efd977ad47c50cf75c3a2c7d19b
Andry Ariadi <andryariadi23@gmail.com>
2023-12-02 13:12:12 +0700

    styling transaction table

--- cms-dashboard/src/components/dashboard/transactions/transaction.jsx
+++ cms-dashboard/src/components/dashboard/transactions/transaction.jsx
@@ -1,7 +1,64 @@
+import styles from "./transaction.module.css";
+import Image from "next/image";
+import user from "@/assets/users.png";
+
 export default function Transaction() {
   return (
     <>
-      <h1>Transaction Components</h1>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Latest Transactions</h2>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Status</td>
+              <td>Date</td>
+              <td>Amount</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image src={user} alt="users" width={50} height={50} className={styles.userImg} />
+                  Andry Ariadi
+                </div>
+              </td>
+              <td>
+                <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+              </td>
+              <td>02.12.2023</td>
+              <td>$2.300</td>
+            </tr>
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image src={user} alt="users" width={50} height={50} className={styles.userImg} />
+                  Andry Ariadi
+                </div>
+              </td>
+              <td>
+                <span className={`${styles.status} ${styles.done}`}>Done</span>
+              </td>
+              <td>02.12.2023</td>
+              <td>$2.300</td>
+            </tr>
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image src={user} alt="users" width={50} height={50} className={styles.userImg} />
+                  Andry Ariadi
+                </div>
+              </td>
+              <td>
+                <span className={`${styles.status} ${styles.cancelled}`}>Cancelled</span>
+              </td>
+              <td>02.12.2023</td>
+              <td>$2.300</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
