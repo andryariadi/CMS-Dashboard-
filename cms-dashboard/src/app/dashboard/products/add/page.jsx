bdb2fc3ad2c5ae6f495f4a3349132daf928da7d7
Andry Ariadi <andryariadi23@gmail.com>
2023-12-03 22:39:41 +0700

    styling add product  component

--- cms-dashboard/src/app/dashboard/products/add/page.jsx
+++ cms-dashboard/src/app/dashboard/products/add/page.jsx
@@ -4,7 +4,21 @@ export default function AddProductPage() {
   return (
     <>
       <div className={styles.container}>
-        <h1>Product Page</h1>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Title" name="title" required />
+          <select name="category" id="category">
+            <option value="general">Choose Category</option>
+            <option value="electronic">Electronic</option>
+            <option value="men's clothing">Men's clothing</option>
+            <option value="women's clothing">Women's clothing</option>
+          </select>
+          <input type="number" placeholder="Price" name="price" required />
+          <input type="number" placeholder="Stock" name="stock" required />
+          <input type="text" placeholder="Color" name="color" required />
+          <input type="text" placeholder="Size" name="size" required />
+          <textarea name="description" id="description" rows="10" placeholder="Description"></textarea>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
