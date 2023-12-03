5e7b0709ab2071b6e3ae906467d262391ab4eb7d
Andry Ariadi <andryariadi23@gmail.com>
2023-12-03 22:41:58 +0700

    add added user page

---
+++ cms-dashboard/src/app/dashboard/users/add/page.jsx
@@ -0,0 +1,25 @@
+import styles from "./add.module.css";
+
+export default function AddUserPage() {
+  return (
+    <>
+      <div className={styles.container}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Title" name="title" required />
+          <select name="category" id="category">
+            <option value="general">Choose Category</option>
+            <option value="electronic">Electronic</option>
+            <option value="men's clothing">Men's clothing</option>
+            <option value="women's clothing">Women's clothing</option>
+          </select>
+          <input type="number" placeholder="Price" name="price" required />
+          <input type="number" placeholder="Stock" name="stock" required />
+          <input type="text" placeholder="Color" name="color" required />
+          <input type="text" placeholder="Size" name="size" required />
+          <textarea name="description" id="description" rows="10" placeholder="Description"></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
