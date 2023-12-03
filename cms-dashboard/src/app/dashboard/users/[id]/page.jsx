de59fa85d35201b74cd9ef3690bcb619e5cc0462
Andry Ariadi <andryariadi23@gmail.com>
2023-12-04 01:05:53 +0700

    styling detail user

--- cms-dashboard/src/app/dashboard/users/[id]/page.jsx
+++ cms-dashboard/src/app/dashboard/users/[id]/page.jsx
@@ -7,30 +7,34 @@ export default function DetailUserPage() {
       <div className={styles.container}>
         <div className={styles.user}>
           <div className={styles.imgContainer}>
-            <Image src={user} alt="User" height={50} width={50} />
-            Andry Ariadi
+            <Image src={user} alt="User" fill />
           </div>
+          Andry Ariadi
         </div>
-        <div className={styles.form}>
-          <label>Username</label>
-          <input type="text" name="username" placeholder="Andry Ariadi" />
-          <label>Email</label>
-          <input type="email" name="email" placeholder="andryariad23@gmail.com" />
-          <label>Password</label>
-          <input type="password" name="password" placeholder="********" />
-          <label>Phone</label>
-          <input type="phone" name="phone" placeholder="123456789" />
-          <textarea name="address" id="" cols="30" rows="10" placeholder="Malang City" />
-          <label>Is Admin ?</label>
-          <select name="isAdmin" id="isAdmin">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
-          </select>
-          <select name="isActive" id="isActive">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
-          </select>
-          <button type="submit">Update</button>
+        <div className={styles.formContainer}>
+          <form action="" className={styles.form}>
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Andry Ariadi" />
+            <label>Email</label>
+            <input type="email" name="email" placeholder="andryariad23@gmail.com" />
+            <label>Password</label>
+            <input type="password" name="password" placeholder="********" />
+            <label>Phone</label>
+            <input type="phone" name="phone" placeholder="123456789" />
+            <label>Address</label>
+            <textarea name="address" id="" cols="30" rows="2" placeholder="Malang City" />
+            <label>Is Admin ?</label>
+            <select name="isAdmin" id="isAdmin">
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+            <label>Is Active ?</label>
+            <select name="isActive" id="isActive">
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+            <button type="submit">Update</button>
+          </form>
         </div>
       </div>
     </>
