04bd6d8c9c0c5c742502026c55863cfb146a6e0e
Andry Ariadi <andryariadi23@gmail.com>
2023-12-03 21:53:39 +0700

    add added product page

---
+++ cms-dashboard/src/app/dashboard/products/add/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./add.module.css";
+
+export default function AddProductPage() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Product Page</h1>
+      </div>
+    </>
+  );
+}
