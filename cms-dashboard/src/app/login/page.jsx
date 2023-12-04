692cbc155006a6873f255c0191f487e199a6fbc8
Andry Ariadi <andryariadi23@gmail.com>
2023-12-04 12:53:03 +0700

    add login page

--- cms-dashboard/src/app/login/page.jsx
+++ cms-dashboard/src/app/login/page.jsx
@@ -1,7 +1,16 @@
+import stlyes from "./login.module.css";
+
 export default function LoginPage() {
   return (
     <>
-      <h1>Login Page</h1>
+      <div className={stlyes.container}>
+        <form action="" className={stlyes.form}>
+          <h1>Login</h1>
+          <input type="text" placeholder="Username" name="username" />
+          <input type="password" placeholder="Password" name="password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 }
