f76be30cbc83a39537aa903d2e428fecd4fd4c03
Andry Ariadi <andryariadi23@gmail.com>
2023-12-06 06:11:44 +0700

    add server action addUser

--- cms-dashboard/src/app/dashboard/users/add/page.jsx
+++ cms-dashboard/src/app/dashboard/users/add/page.jsx
@@ -1,10 +1,11 @@
+import { addUser } from "@/libs/actions";
 import styles from "./addUser.module.css";
 
 export default function AddUserPage() {
   return (
     <>
       <div className={styles.container}>
-        <form action="" className={styles.form}>
+        <form action={addUser} className={styles.form}>
           <input type="text" placeholder="Username" name="username" required />
           <input type="email" placeholder="Email" name="email" required />
           <input type="password" placeholder="Password" name="password" required />
---
+++ cms-dashboard/src/libs/actions.js
@@ -0,0 +1,56 @@
+"use server";
+
+import { User } from "./models";
+import { connectToDB } from "./utility";
+
+export const addUser = async (formData) => {
+  const { username, email, password, phone, isAdmin, isActive, address } = formData;
+
+  try {
+    connectToDB();
+    // if (!username || !email || !password) {
+    //   throw new Error("Please fill in all fields!");
+    // }
+
+    // if (password.length < 6) {
+    //   throw new Error("Password must be at least 6 characters long!");
+    // }
+
+    // const existingUser = await User.findOne({ email });
+
+    // if (existingUser) {
+    //   throw new Error("User already exists!");
+    // }
+
+    // if (phone && phone.length !== 10) {
+    //   throw new Error("Phone number must be 10 digits long!");
+    // }
+
+    // if (address && address.length < 10) {
+    //   throw new Error("Address must be at least 10 characters long!");
+    // }
+
+    // if (isAdmin === "false" && address) {
+    //   throw new Error("User cannot have an address if not admin!");
+    // }
+
+    // if (isActive === "false" && phone) {
+    //   throw new Error("User cannot have a phone number if not active!");
+    // }
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+      phone,
+      isAdmin,
+      isActive,
+      address,
+    });
+
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to add user!");
+  }
+};
