cb3dc7b944b5e9d82aa9a9b7ab2f65aa905f5d67
Andry Ariadi <andryariadi23@gmail.com>
2023-12-11 07:04:22 +0700

    update layout

--- cms-dashboard/src/app/layout.js
+++ cms-dashboard/src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Dashboard CMS",
-  description: "Dashboard CMS with Next.js",
+  description: "Andry Ariadi Dashboard CMS with Next.js",
 };
 
 export default function RootLayout({ children }) {
